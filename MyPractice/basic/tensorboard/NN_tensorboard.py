425c4c3979e6ae163739faff5b30039fd5744c7f
Ouyang <1305805709@qq.com>
2019-08-02 12:55:16 +0800

    practice1.0

--- MyPractice/basic/tensorboard/NN_tensorboard.py
+++ MyPractice/basic/tensorboard/NN_tensorboard.py
@@ -1,4 +1,3 @@
-#-*- coding : GBK -*-
 import tensorflow as tf
 import numpy as np
 """
@@ -64,7 +63,7 @@ with tf.Session() as sess:
 """
 我们使用命令来查看最后的结果：
 
-tensorboard --logdir logs
+tensorboard --logdir=F:\推荐系统\DeepLearning\MyPractice\basic\tensorboard\logs 需要完整路径
+然后我们就可以根据它提示的网址去访问我们的结果啦，用chrome是可以的呦。或者直接在浏览器做输入 localhost:6006
 
-然后我们就可以根据它提示的网址去访问我们的结果啦，用chrome是可以的呦。
 """
